2e6539b3f8fa565327c2e42f1a39cce2c5f581e2
Qusai Altoubasy <qaltwbasy@gmail.com>
2025-11-19 01:00:55 +0300

    19.11.2025^

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,5 @@
-// Qusai Altoubasy
-
 import 'package:flutter/material.dart';
-import 'package:qusai/screens/admin/admin_main_screen.dart';
-import 'package:qusai/screens/charity/charity_main_screen.dart';
-import 'package:qusai/screens/base_screens/login_screen.dart';
 import 'package:qusai/screens/base_screens/logo_screen.dart';
-import 'package:qusai/screens/common_screens/another_profile.dart';
-import 'package:qusai/screens/register/charity_register.dart';
-import 'package:qusai/screens/register/register_options.dart';
-import 'package:qusai/screens/register/user_register.dart';
-import 'package:qusai/screens/temp.dart';
-import 'package:qusai/screens/user/user_layout.dart';
 
 void main() {
   runApp(const MyApp());
